2d5f4a4abd0c24101ed866413aa989b551378a88
Kaj Bostrom <bostromk@cs.washington.edu>
2017-10-26 00:39:03 -0700

    tear down virtualenv

---
+++ pasta.py
@@ -0,0 +1,7 @@
+import json
+
+def load(json_filename):
+    with open(json_filename) as f:
+        text_list = json.load(f)
+    tokenizer = allennlp.data.tokenizers.word_tokenizer.WordTokenizer()
+    return list(map(tokenizer.tokenize(text_list)))
