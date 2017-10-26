db355626d5251e54e685a853d4f35faa42dd5d67
Kaj Bostrom <bostromk@cs.washington.edu>
2017-10-26 01:40:24 -0700

    fix

--- pasta.py
+++ pasta.py
@@ -1,7 +1,8 @@
+import allennlp.data
 import json
 
 def load(json_filename):
     with open(json_filename) as f:
         text_list = json.load(f)
     tokenizer = allennlp.data.tokenizers.word_tokenizer.WordTokenizer()
-    return list(map(tokenizer.tokenize(text_list)))
+    return list(map(tokenizer.tokenize, text_list))
