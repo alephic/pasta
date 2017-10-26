94ae69cfc210ed917f96c88a2e0c04ba625a710d
Kaj Bostrom <bostromk@cs.washington.edu>
2017-10-26 16:51:12 -0700

    make Dataset out of loaded text

--- pasta.py
+++ pasta.py
@@ -1,8 +1,21 @@
-import allennlp.data
 import json
+
+from allennlp.data.fields import TextField
+from allennlp.data import Dataset, Instance, Vocabulary
+from allennlp.data.token_indexers import SingleIdTokenIndexer, TokenCharactersIndexer
+from allennlp.data.tokenizers.word_splitter import SpacyWordSplitter
 
 def load(json_filename):
     with open(json_filename) as f:
         text_list = json.load(f)
-    tokenizer = allennlp.data.tokenizers.word_tokenizer.WordTokenizer()
-    return list(map(tokenizer.tokenize, text_list))
+    splitter = SpacyWordSplitter()
+    indexers = {'tokens': SingleIdTokenIndexer(), 'token_characters': TokenCharactersIndexer()}
+    dataset = Dataset([
+        Instance({
+            'text': TextField(
+                tokens=splitter.split_words(text)[0], 
+                token_indexers=indexers
+            )
+        }) for text in text_list
+    ])
+    return dataset
