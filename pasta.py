32279e90362fc8cd188dc616221a0a178adc6695
Kaj Bostrom <bostromk@cs.washington.edu>
2017-10-28 04:23:35 -0700

    wip refactor forward

--- pasta.py
+++ pasta.py
@@ -3,7 +3,7 @@ import json
 from allennlp.data.fields import TextField
 from allennlp.data import Dataset, Instance, Vocabulary
 from allennlp.data.token_indexers import SingleIdTokenIndexer, TokenCharactersIndexer
-from allennlp.data.tokenizers.word_splitter import SpacyWordSplitter
+from allennlp.data.tokenizers import WordTokenizer
 from allennlp.models.model import Model
 from allennlp.modules.token_embedders.embedding import Embedding
 from allennlp.modules.token_embedders import TokenCharactersEncoder
@@ -15,7 +15,7 @@ from highway_lstm.highway_lstm_layer import HighwayLSTMLayer
 
 import torch.nn as nn
 import torch
-from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence, PackedSequence
 from torch.autograd import Variable
 
 import numpy as np
@@ -27,12 +27,12 @@ from dropout_embedding import DropoutEmbedding
 def load(json_filename):
     with open(json_filename) as f:
         text_list = json.load(f)
-    splitter = SpacyWordSplitter()
+    tokenizer = WordTokenizer(start_tokens=['@@SOS@@'])
     indexers = {'tokens': SingleIdTokenIndexer(), 'token_characters': TokenCharactersIndexer()}
     dataset = Dataset(list(tqdm((
         Instance({
             'text': TextField(
-                tokens=splitter.split_words(text), 
+                tokens=tokenizer.tokenize(text),
                 token_indexers=indexers
             )
         }) for text in text_list
@@ -85,20 +85,26 @@ class PastaEncoder(Model):
             nn.ReLU(),
             nn.Linear(dist_mlp_hidden_size, 2*latent_size)
         )
-        self.word_dec_lstm_cells = nn.ModuleList([
-            nn.modules.LSTMCell(latent_size, word_lstm_size) for i in range(word_lstm_layers)
-        ])
+        self.word_dec_lstm = HighwayLSTMLayer(
+            self.text_field_embedder.get_output_dim()+latent_size,
+            word_lstm_size,
+            num_layers=word_lstm_layers,
+            recurrent_dropout_prob=word_lstm_dropout
+        )
+        self.word_dec_project_out_size = vocab.get_vocab_size(namespace='tokens') + 1 # extra slot for EOS
         self.word_dec_project = nn.Linear(
             word_lstm_size,
-            vocab.get_vocab_size(namespace='tokens')
+            self.word_dec_project_out_size
         )
-        self.char_dec_lstm_cell = nn.modules.LSTMCell(word_lstm_size, char_lstm_size)
-        self.char_dec_project = nn.Linear(
-            char_lstm_size,
-            vocab.get_vocab_size(namespace='token_characters')
-        )
-        # GPU-only
+        #self.char_dec_lstm_cell = nn.modules.LSTMCell(word_lstm_size, char_lstm_size)
+        #self.char_dec_project = nn.Linear(
+        #    char_lstm_size,
+        #    vocab.get_vocab_size(namespace='token_characters') + 1 # extra slot for EOS
+        #)
+
+        # Model is GPU-only
         self.cuda()
+
     def do_word_dropout(self, token_indices_array):
         if self.training:
             token_mask = (token_indices_array != 0).astype(int)
@@ -106,23 +112,38 @@ class PastaEncoder(Model):
             return (token_indices_array * (1-drop_mask)) + token_mask*drop_mask
         else:
             return token_indices_array
-    def get_latent_dist_params(self, data: Dataset):
-        lengths_list = [len(instance.fields['text'].tokens) for instance in data.instances]
-        lengths_var = Variable(torch.cuda.LongTensor(lengths_list), requires_grad=False)
-        batch_size = len(data.instances)
+    
+    def embed_inputs(self, data: Dataset):
         array_dict = data.as_array_dict()
-        embedded = self.text_field_embedder({
+        return self.text_field_embedder({
             'tokens': Variable(torch.cuda.LongTensor(self.do_word_dropout(array_dict['text']['tokens'])), requires_grad=False),
             'token_characters': Variable(torch.cuda.LongTensor(array_dict['text']['token_characters']), requires_grad=False)
         })
-        embedded_sorted, lengths_sorted, restoration_indices = sort_batch_by_length(embedded, lengths_var)
-        packed = pack_padded_sequence(embedded_sorted, lengths_sorted.data.tolist(), batch_first=True)
-        word_enc_out = self.word_enc_lstm(packed)[0]
+
+    def get_latent_dist_params(self, inputs, input_lengths: Variable):
+        word_enc_out = self.word_enc_lstm(packed_inputs)[0]
         padded = pad_packed_sequence(word_enc_out, batch_first=True)[0]
-        padded = padded.index_select(0, restoration_indices)
-        last_indices = (lengths_var - 1).view(batch_size, 1, 1).expand(-1, -1, padded.size()[2])
+        last_indices = (input_lengths - 1).view(padded.size()[0], 1, 1).expand(-1, -1, padded.size()[2])
         encodings = padded.gather(1, last_indices).squeeze(1)
-        return self.dist_mlp(encodings)
+        return self.dist_mlp(encodings).chunk(2, dim=1)
+
+    def get_reconstruction_logits(self, packed_inputs):
+        word_dec_out = self.word_dec_lstm(packed_inputs)[0]
+        packed_logits = PackedSequence(self.word_dec_project(word_dec_out.data), word_dec_out.batch_sizes)
+        return pad_packed_sequence(packed_logits, batch_first=True)[0]
+
+    def get_negative_kl_divergence_from_normal(mu, sigma):
+        sigma_squared = sigma ** 2
+        return 0.5 * torch.sum(1 + torch.log(sigma_squared) - mu**2 - sigma_squared)
+
+    def forward(self, data: Dataset):
+        embedded = self.embed_inputs(data)
+        lengths = [len(instance.fields['text'].tokens) for instance in data]
+        lengths_var = Variable(torch.cuda.LongTensor(lengths), requires_grad=False)
+        sorted_embedded, sorted_lengths, unsort_indices = sort_batch_by_length(embedded, lengths_var)
+        packed_inputs = pack_padded_sequence(sorted_embedded, sorted_lengths, batch_first=True)
+        mu, sigma = self.get_latent_dist_params(packed_inputs, sorted_lengths)
+        sampled_latents = torch.normal(mu, sigma)
 
 def test_encode():
     d = load('data/emojipasta.json')
